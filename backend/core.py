45ce12a899705b6fbe94a8a9d55dc42474c500fc
SM Sadat <smsadat788@gmail.com>
2026-01-04 21:10:46 +0600

    fix: Remove test artifacts in backend

--- backend/core.py
+++ backend/core.py
@@ -1,10 +1,11 @@
 from summarizer import summarize
 from transcriber import transcribe
 
-def audio_to_text_summ(filepath : str):
+def audio_to_text_summ(filepath : str) -> str:
 
+    sum : str = ""
+    
     trs : str = transcribe('/home/pancake/Music/starboy-weeknd.mp3', 'base')
-    print(trs)
-
     sum = summarize(trs)
-    print(sum)
+
+    return sum
